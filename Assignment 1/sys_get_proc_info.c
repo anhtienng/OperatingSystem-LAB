49cb188d865abf488d0ad2b6a353ea2e58efd352
Anh Nguyen <anhnguyen@Anhs-MacBook-Pro.local>
2019-10-01 20:12:22 +0700

    upload

--- Assignment 1/sys_get_proc_info.c
+++ Assignment 1/sys_get_proc_info.c
@@ -25,30 +25,24 @@ void create_procinfos(struct task_struct *process, struct procinfos __user *info
     
     memset(&result, 0, sizeof(struct procinfos));
     result.studentID = 1752076;
-    //printk(KERN_INFO "create_procinfos is called");
 
     // current process
     (result.proc).pid = process->pid;
     get_task_comm((result.proc).name, process);
-    //printk(KERN_INFO "sys_get_proc_info: Done current");
 
     // parent process
     parent_process = process->parent;
     if (parent_process){
         (result.parent_proc).pid = parent_process->pid;
         get_task_comm((result.parent_proc).name, parent_process);
-        //printk(KERN_INFO "sys_get_proc_info: Done parent");
     }
 
     // oldest_child
     children_list = &process->children;
     
     // if there are no children
-    if (list_empty(children_list)){
-        //handle no child process
-        oldest_child_process = NULL;
-        //printk(KERN_INFO "sys_get_proc_info: No child");
-    }
+    if (list_empty(children_list))
+        oldest_child_process = NULL; //handle no child process
     else
         oldest_child_process = list_first_entry(children_list, struct task_struct, sibling);
     
@@ -65,22 +59,19 @@ void create_procinfos(struct task_struct *process, struct procinfos __user *info
 
 SYSCALL_DEFINE2 (get_proc_info, pid_t, pid, struct procinfos __user *, info){
     struct task_struct *process;
-    printk (KERN_INFO "%d, sys_get_proc_info is invoked", 1752076);
     if (pid == -1){
         //current process
         process = current;
-        //printk(KERN_INFO "pid: %d", process->pid);
         create_procinfos(process, info);
     }
     else if (pid >= 0){
         process = find_task_by_vpid(pid);
         if (process == NULL)
-            return -EINVAL;
-        //printk(KERN_INFO "pid: %d", process->pid);
+            return EINVAL;
         create_procinfos(process, info);
     }
     else
-        return -EpINVAL;
+        return EINVAL;
     
     return 0;
 }
